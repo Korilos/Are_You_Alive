7cc0cc2334ebe3a5009dd1a0564af83966ec4ced
Korilos <47279084+Korilos@users.noreply.github.com>
2024-09-24 20:42:05 +0300

    Add files via upload

---
+++ src/components/ui/alert-dialog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AlertDialog = ({ children }) => <div>{children}</div>;
+export const AlertDialogAction = ({ children }) => <button>{children}</button>;
+export const AlertDialogContent = ({ children }) => <div>{children}</div>;
+export const AlertDialogDescription = ({ children }) => <p>{children}</p>;
+export const AlertDialogFooter = ({ children }) => <div>{children}</div>;
+export const AlertDialogHeader = ({ children }) => <div>{children}</div>;
+export const AlertDialogTitle = ({ children }) => <h1>{children}</h1>;
