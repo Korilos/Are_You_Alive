27a7fa7721c8f6b26092ec85e0fd9dac32a6b290
Korilos <47279084+Korilos@users.noreply.github.com>
2024-09-24 20:59:42 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// src/index.js or src/App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Import Tailwind CSS
+import App from './App';
+import './src/styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
